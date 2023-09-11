d430016c2872b5635c004db25f77989f684d119e
yiyanglu1 <yiyang.lu@mail.utoronto.ca>
2023-09-11 17:55:55 -0400

    dofizzbuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,31 +4,37 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
-
-        for (int i = 1; i < 100; i++) {
+        int j = 1;
+        while (j < 100){
 
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+            j = dofizzbuzz(j);
         }
+    }
+
+    private static int dofizzbuzz(int j) {
+        boolean divisibleBy3 = j % 3 == 0;
+        boolean divisibleBy5 = j % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(j);
+
+        }
+        j++;
+        return j;
     }
 }
